82f2e816a3248bc66fd80cc863fe315a99992ceb
Leonardo Porras González <lporras@bolsacr.com>
2020-05-25 22:14:36 -0600

    Insertar registros y Obtener registros SQFLite

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:path/path.dart';
+import 'package:qrreaderapp/src/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -14,7 +15,7 @@ class DBProvider {
 
   Future <Database> get database async{
     //Consultamos si ya existe la base de datos
-    if (_database != null) return _database;
+    if (_database != null) return _database; // En caso de que ya exista la DB, nos devuelve esa DB
     // De lo contrario creamos la base de datos
     _database = await initDB();
 
@@ -44,4 +45,56 @@ class DBProvider {
     );
     
   }
+
+  //CREAR Registros
+  nuevoScanRaw(ScanModel nuevoScan) async {//Aqui tengo toda la información de lo que se escaneo
+    final db = await database;
+    final res = await db.rawInsert(
+      "INSERT Into Scans (id, tipo, valor) "//Dejamos un espacio al final
+      "VALUES (${ nuevoScan.id }, '${ nuevoScan.tipo}', '${ nuevoScan.valor }')"
+    );
+    return res;
+  }
+
+  //Este metodo hace lo mismo que el anterior, es mas sencillo.
+  //Pero es importante conocer el modelo anterior
+  //Tambien con este modelo es mas rapido, con el anterior tendriamos que escribir cada inserción
+  nuevoScan(ScanModel nuevoScan) async { 
+    final db = await database; 
+    final res = await db.insert('Scans', nuevoScan.toJson()); // Esta es mas segura que la anterior
+    return res; 
+  }
+
+  //Select - Obtener información
+  Future<ScanModel> getScanId(int id) async {
+    final db = await database;
+    //Signo de pregunta en los Where significa que tiene que ser un argumento
+    final res = await db.query('Scans', where: 'id=?', whereArgs: [id]);
+    //Esto va a retornar un lista de mapas(json), ocupo el primer elemento
+    return res.isNotEmpty ? ScanModel.fromJson(res.first) : null;
+  }
+
+  //Si necesito traer todos los scans
+  Future <List<ScanModel>> getTodosScans() async {
+    final db = await database;
+    final res = await db.query('Scans');
+
+    List<ScanModel> list = res.isNotEmpty //Si esto no esta vacio
+                              ? res.map((e) => ScanModel.fromJson(e)).toList() //Va a proceder a crear instancias de ScanModel
+                              :[]; //En caso contratrio va a retornar una lista vacia
+    return list;
+
+  }
+Future <List<ScanModel>> getScansPorTipo(String tipo) async {
+    final db = await database;
+    final res = await db.rawQuery("SELECT * FROMS Scans WHERE tipo='$tipo'");
+
+    List<ScanModel> list = res.isNotEmpty //Si esto no esta vacio
+                              ? res.map((e) => ScanModel.fromJson(e)).toList() //Va a proceder a crear instancias de ScanModel
+                              :[]; //En caso contratrio va a retornar una lista vacia
+    return list;
+
+  }
+  // Si necesito retornar una lista por tipo
+
 }
