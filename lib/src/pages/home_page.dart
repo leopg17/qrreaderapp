c04732078e2544a1010884d02a30b0157c944c23
Leonardo Porras González <lporras@bolsacr.com>
2020-05-27 22:24:12 -0600

    Actualizando los widget mediante stream

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+
+import 'package:qrreaderapp/src/bloc/scans_bloc.dart';
 //import 'package:barcode_scan/barcode_scan.dart';
 import 'package:qrreaderapp/src/models/scan_model.dart';
+
 import 'package:qrreaderapp/src/pages/direcciones_page.dart';
 import 'package:qrreaderapp/src/pages/mapas_page.dart';
-import 'package:qrreaderapp/src/providers/db_provider.dart';
+//No debemos de utilizar el DB Provider aca, esto para centralizar toda la información
+//y que se notifique correctamente, DBProvider solo va a ser accedida mediante el Bloc
+//mport 'package:qrreaderapp/src/providers/db_provider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,6 +17,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final scansBloc = new ScansBloc();
 
   int currentIndex = 0;
 
@@ -24,7 +31,9 @@ class _HomePageState extends State<HomePage> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.delete_forever),
-            onPressed: (){},
+            onPressed: (){
+              scansBloc.borrarScanTODOS();
+            },
             
           )
         ]
@@ -60,7 +69,7 @@ class _HomePageState extends State<HomePage> {
 
     if(futureString != null){
       final scan = ScanModel(valor: futureString);
-      DBProvider.db.nuevoScan(scan);
+      scansBloc.agregarScan(scan);
     }
   }
 
--- lib/src/pages/mapas_page.dart
+++ lib/src/pages/mapas_page.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:qrreaderapp/src/providers/db_provider.dart';
+
+import 'package:qrreaderapp/src/bloc/scans_bloc.dart';
+import 'package:qrreaderapp/src/models/scan_model.dart';
+
 
 class MapasPage extends StatelessWidget {
+
+  final scansBloc = new ScansBloc();
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<ScanModel>>(
-      future: DBProvider.db.getTodosScans(),
+
+
+    return StreamBuilder<List<ScanModel>>(
+      stream: scansBloc.scansStream,
       builder: (BuildContext context, AsyncSnapshot<List<ScanModel>> snapshot){
         if(!snapshot.hasData){
           return Center(
@@ -24,7 +31,7 @@ class MapasPage extends StatelessWidget {
           itemBuilder: (context, i) => Dismissible(
             key: UniqueKey(),
             background: Container(color: Colors.red),
-            onDismissed: (direction) => DBProvider.db.deleteScan(scans[i].id),
+            onDismissed: (direction) => scansBloc.borrarScan(scans[i].id),
             child: ListTile(
               leading: Icon(Icons.cloud_queue, color: Theme.of(context).primaryColor),
               title: Text(scans[i].valor),
